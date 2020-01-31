62a4c23125784f1277b65c5b14de6ba9e574c83a
Zenahr <zenmatica@gmail.com>
2020-01-31 21:41:32 +0100

    init

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 
-// TweenMax.from(".hero", 2.5, {
-//       opacity: 0,
-//       ease: Power2.easeIn
-// });
+TweenMax.from(".hero", 2.5, {
+      opacity: 0,
+      ease: Power2.easeIn
+});
 
-// t1 = new TimelineMax();
+t1 = new TimelineMax();
 
-// TweenMax.to(".hero", 2.5, {
-//       delay: 2,
-// })
+TweenMax.to(".hero", 2.5, {
+      delay: 2,
+})
 
-// TweenMax.from(".hero-headline", 2, {
-//       delay: 1,
-//       opacity: 0,
-//       ease: Power2.easeIn
+TweenMax.from(".hero-headline", 2, {
+      delay: 1,
+      opacity: 0,
+      ease: Power2.easeIn
 
-// })
+})
 
-// TweenMax.from(".hero-tagline", 1.5, {
-//       delay: 2.5,
-//       opacity: 0,
-//       ease: Power2.easeIn
+TweenMax.from(".hero-tagline", 1.5, {
+      delay: 2.5,
+      opacity: 0,
+      ease: Power2.easeIn
 
-// })
+})
 
-// TweenMax.from(".card", 1.5, {
-//       delay: 2.5,
-//       opacity: 0,
-//       ease: Power2.easeIn
+TweenMax.from(".card", 1.5, {
+      delay: 2.5,
+      opacity: 0,
+      ease: Power2.easeIn
 
-// })
+})
 
